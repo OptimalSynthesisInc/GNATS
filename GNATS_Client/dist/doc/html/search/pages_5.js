2245b0e9cd88a83285276bb26de22728bbbab799
oliverchen <oliverchen@Linux-PEGASUS>
2021-01-18 16:52:14 -0800

    Removed

--- GNATS_Client/dist/doc/html/search/pages_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['installation_20of_20gnats_20client',['Installation of GNATS Client',['../installation.html',1,'']]],
-  ['input_20_26_20output',['Input &amp; Output',['../io.html',1,'']]]
-];
